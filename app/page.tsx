9a3a2c5bfda40bbea106c9a12af81ee157bf12cd
Randy <randy930529@gmail.com>
2025-10-04 00:04:08 -0600

    feat: agrega el componente para el botón de serrar sección

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import VitalGestLogo from "@/app/ui/vitalgest-logo";
+import ButtonSignout from "@/app/ui/button-signout";
 
 export default function Page() {
   return (
@@ -6,7 +7,9 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <VitalGestLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row"></div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <ButtonSignout />
+      </div>
     </main>
   );
 }
