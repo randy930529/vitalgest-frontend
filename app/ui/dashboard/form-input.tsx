d20002e51e2faf4e502451c990f1f14236690612
Randy <randy930529@gmail.com>
2025-10-18 00:04:49 -0600

    feat: agrega los datos del encabesado de la sección de ambulancias y un nuebo campo al input de formulario para cargar un valor inicial si es necesario

--- app/ui/dashboard/form-input.tsx
+++ app/ui/dashboard/form-input.tsx
@@ -4,9 +4,11 @@ import { InlineErrors } from "@/app/ui/custom-errors";
 export function FormInput({
   name,
   errors,
+  initialValue,
   customFormInput,
 }: {
   name: string;
+  initialValue?: string;
   errors?: string[];
   customFormInput: FormInputType;
 }) {
@@ -25,6 +27,7 @@ export function FormInput({
         type={type}
         name={name}
         id={name}
+        defaultValue={initialValue || ""}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         placeholder={placeholder || title}
         required={required}
