54e41347d64f09dcfec6f582a328fb26b42e3cdb
Randy <randy930529@gmail.com>
2025-10-23 01:57:57 -0600

    feat: agrega un selector de las delegaciones para los formularios

---
+++ app/ui/dashboard/delegations/delegations-selector.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import toast from "react-hot-toast";
+import { DelegationType } from "@/app/lib/definitions";
+import { FormSelect } from "@/app/ui/dashboard/form-fields";
+
+export default function DelegationsSelector({
+  delegations,
+  errors,
+}: {
+  delegations: DelegationType[];
+  errors?: string[];
+}) {
+  // Selector interactivo de delegaciones - [CSR]
+  const showError = useRef(false);
+
+  const customSelectedDelegations = delegations.map(
+    ({ id, state, municipality }, index) => ({
+      id: index + 1,
+      value: id,
+      label: state.name + " - " + municipality.name,
+    })
+  );
+  customSelectedDelegations.unshift({
+    id: 0,
+    value: "",
+    label: "Seleccione la Delegación",
+  });
+
+  useEffect(() => {
+    if (delegations?.length === 0 && !showError.current) {
+      toast.error("Debe registrar al menos una delegación.");
+      showError.current = true;
+    }
+  }, [delegations]);
+
+  return (
+    <FormSelect
+      name="delegation"
+      title="Delegación"
+      options={customSelectedDelegations}
+      errors={errors}
+      required
+    />
+  );
+}
