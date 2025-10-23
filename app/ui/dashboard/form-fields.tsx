b7fff080288b2b82dc9de6289cf4d0600a507ecd
Randy <randy930529@gmail.com>
2025-10-23 01:57:03 -0600

    feat: agrega manejos de inavilitación y cambio al campo de selección del formulario

--- app/ui/dashboard/form-fields.tsx
+++ app/ui/dashboard/form-fields.tsx
@@ -50,6 +50,8 @@ export function FormSelect({
   defaultValue,
   errors,
   required,
+  disabled,
+  handleOption,
 }: {
   name: string;
   title?: string;
@@ -58,9 +60,11 @@ export function FormSelect({
     value: string;
     label: string;
   }[];
-  defaultValue?: string;
+  defaultValue?: string | number;
   errors?: string[];
   required?: boolean;
+  disabled?: boolean;
+  handleOption?: (name: string, value: string) => void;
 }) {
   return (
     <div>
@@ -79,6 +83,10 @@ export function FormSelect({
         defaultValue={defaultValue}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
         required={required}
+        disabled={disabled}
+        onChange={(e) =>
+          handleOption && handleOption(e.target.name, e.target.value)
+        }
       >
         {options.map(({ id, value, label }) => (
           <option key={id} value={value}>
