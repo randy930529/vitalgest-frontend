de134eb0381875387f41a2b3a5e105f252dd4f18
Randy <randy930529@gmail.com>
2025-10-29 22:38:15 -0600

    feat: agrega un componente de checkbox para entradas de formulario

--- app/ui/dashboard/form-fields.tsx
+++ app/ui/dashboard/form-fields.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FormInputType } from "@/app/lib/definitions";
 import { InlineErrors } from "@/app/ui/custom-errors";
 
@@ -102,5 +103,37 @@ export function FormSelect({
         />
       )}
     </div>
+  );
+}
+
+export function FormCheckbox({
+  name,
+  title,
+  isChecked,
+}: {
+  name: string;
+  title: string;
+  isChecked: boolean;
+}) {
+  const [checked, setChecked] = useState(isChecked);
+
+  return (
+    <label
+      htmlFor={name}
+      className="inline-flex items-center me-5 cursor-pointer"
+    >
+      <input
+        id={name}
+        type="checkbox"
+        name={name}
+        className="sr-only peer"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600 dark:peer-checked:bg-green-600"></div>
+      <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+        {title}
+      </span>
+    </label>
   );
 }
