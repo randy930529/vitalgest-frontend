c537fad47c3622340ff2e28f405b5dce66125b86
Randy <randy930529@gmail.com>
2025-11-01 00:49:13 -0600

    feat: agrega manejo de loading a la ruta de guardias utilizando el componente Suspense

--- app/dashboard/guards/page.tsx
+++ app/dashboard/guards/page.tsx
@@ -1,6 +1,15 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
-import GuardsTable from "@/app/ui/dashboard/guards/guards-table";
+import {
+  fetchDelegations,
+  fetchGuards,
+  fetchUsersGuardChief,
+} from "@/app/lib/data";
 import Breadcrumbs from "@/app/ui/breadcrumbs";
+import GuardsTable from "@/app/ui/dashboard/guards/guards-table";
+import { TableSkeleton } from "@/app/ui/dashboard/skeletons";
+import { WrapperTable } from "@/app/ui/dashboard/wrappers";
+import GuardForm from "@/app/ui/dashboard/guards/create/guard-form";
 
 export const metadata: Metadata = {
   title: "Gestión de Guardias",
@@ -8,6 +17,13 @@ export const metadata: Metadata = {
 
 export default async function GuardsPage() {
   // (Página) Gestionar guardias - [SSR]
+
+  const fetchGuardsGuardChiefsAndDelegations = async () =>
+    await Promise.all([
+      fetchGuards(),
+      fetchUsersGuardChief(),
+      fetchDelegations(),
+    ]);
 
   return (
     <>
@@ -17,7 +33,19 @@ export default async function GuardsPage() {
           { label: "Guardias", href: "/dashboard/guards" },
         ]}
       />
-      <GuardsTable />
+      <Suspense
+        fallback={
+          <TableSkeleton
+            title="Crear Guardia"
+            modelContent={<GuardForm customGuardChief={[]} delegations={[]} />}
+          />
+        }
+      >
+        <WrapperTable
+          fetchData={fetchGuardsGuardChiefsAndDelegations}
+          WrappedComponent={GuardsTable}
+        />
+      </Suspense>
     </>
   );
 }
