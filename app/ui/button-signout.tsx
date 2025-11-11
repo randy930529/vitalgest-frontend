6911c87ae5317fd79f25d513bf20fea0806941cb
Randy <randy930529@gmail.com>
2025-11-11 01:31:32 -0600

    factory: modifica texto del boton salir a cerrar sesion

--- app/ui/button-signout.tsx
+++ app/ui/button-signout.tsx
@@ -14,7 +14,7 @@ export function ButtonSignout() {
     <form action={formSignOutAction}>
       <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
         <PowerIcon className="w-6" />
-        <div className="hidden md:block">Salir</div>
+        <div className="hidden md:block">Cerrar sesión</div>
       </button>
     </form>
   );
@@ -29,7 +29,7 @@ export function LinkSignout() {
   return (
     <form action={formSignOutAction}>
       <button className="block w-full py-2 px-4 text-sm text-left hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-        Salir
+        Cerrar sesión
       </button>
     </form>
   );
