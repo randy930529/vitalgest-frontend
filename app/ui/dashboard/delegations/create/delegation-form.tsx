059b0bab6163f8145965ecfd3fe53b4f5e58a41a
Randy <randy930529@gmail.com>
2026-02-14 02:05:02 -0600

    feat: agrega el componente de formulario genérico y refactorizar múltiples formularios para utilizarlo

--- app/ui/dashboard/delegations/create/delegation-form.tsx
+++ app/ui/dashboard/delegations/create/delegation-form.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { useActionState, useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { createDelegation } from "@/app/lib/actions/delegation";
 import {
-  createDelegation,
-  DelegationState,
-} from "@/app/lib/actions/delegation";
-import { CustomMxState, CustomOptions } from "@/app/lib/definitions";
-import { Button } from "@/app/ui/button";
+  CustomMxState,
+  CustomOptions,
+  FormFieldType,
+} from "@/app/lib/definitions";
 import { FormSelect } from "@/app/ui/dashboard/form-fields";
 import { getMunicipalitiesOfState } from "@/app/lib/utils";
+import { GenericForm } from "@/app/ui/components/GenericForm";
+import { getFormConfigFields } from "@/app/lib/config/formConfigs";
 
 export default function DelegationForm({
   customMxStates,
@@ -18,11 +19,27 @@ export default function DelegationForm({
   customMxStates: CustomMxState[];
   onClose?: () => void;
 }) {
-  // <div>(Component) Formulario de delegacion - [CSR]</div>
+  // (Component) Formulario de delegacion - [CSR]
 
-  const initialState: DelegationState = { errors: {}, message: null };
-  const [state, formAction] = useActionState(createDelegation, initialState);
+  const fields = getFormConfigFields("delegation", [
+    {
+      type: "custom",
+      name: "name",
+      component: CustomFiles({ customMxStates }),
+    },
+  ]);
 
+  return (
+    <GenericForm
+      fields={fields}
+      onSubmit={createDelegation}
+      initialState={{ errors: {}, message: null }}
+      onCancel={onClose}
+    />
+  );
+}
+
+function CustomFiles({ customMxStates }: { customMxStates: CustomMxState[] }) {
   const [mxStateId, setMxStateId] = useState("");
   const [customMunicipalities, setCustomMunicipalities] = useState<
     CustomOptions[]
@@ -30,74 +47,45 @@ export default function DelegationForm({
 
   useEffect(() => {
     setCustomMunicipalities(
-      getMunicipalitiesOfState(mxStateId, customMxStates)
+      getMunicipalitiesOfState(mxStateId, customMxStates),
     );
   }, [mxStateId]);
-
-  useEffect(() => {
-    state.message && toast.success(state.message);
-    state.message && onClose && onClose();
-  }, [state.message]);
-
-  useEffect(() => {
-    state.errors?.success &&
-      state.errors?.success.map((error: string) => toast.error(error));
-  }, [state.errors?.success]);
 
   function handleOption(name: string, value: string) {
     setMxStateId(value);
   }
 
   return (
-    <form action={formAction}>
-      <div className="grid gap-4 mb-4 sm:grid-cols-1">
-        <FormSelect
-          key="state"
-          name="state"
-          title="Estado"
-          defaultValue=""
-          options={[
-            { id: 0, label: "Seleccione el Estado", value: "" },
-            ...customMxStates,
-          ]}
-          handleOption={handleOption}
-          errors={state.errors?.state}
-          required
-        />
+    <>
+      <FormSelect
+        key="state"
+        name="state"
+        title="Estado"
+        defaultValue=""
+        options={[
+          { id: 0, label: "Seleccione el Estado", value: "" },
+          ...customMxStates,
+        ]}
+        handleOption={handleOption}
+        required
+      />
 
-        <FormSelect
-          key="municipality"
-          name="municipality"
-          title="Municipio"
-          defaultValue=""
-          options={[
-            {
-              id: 0,
-              label: "Seleccione el Municipio",
-              value: "",
-            },
-            ...customMunicipalities,
-          ]}
-          errors={state.errors?.municipality}
-          required
-          disabled={!mxStateId}
-        />
-      </div>
-      <div className="w-full flex justify-end gap-4">
-        <Button
-          type="reset"
-          onMouseDown={onClose}
-          className="text-white inline-flex items-center bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Cancelar
-        </Button>
-        <Button
-          type="submit"
-          className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Guardar
-        </Button>
-      </div>
-    </form>
+      <FormSelect
+        key="municipality"
+        name="municipality"
+        title="Municipio"
+        defaultValue=""
+        options={[
+          {
+            id: 0,
+            label: "Seleccione el Municipio",
+            value: "",
+          },
+          ...customMunicipalities,
+        ]}
+        required
+        disabled={!mxStateId}
+      />
+    </>
   );
 }
--- app/ui/dashboard/supplies/ambulances/create/supply-form.tsx
+++ app/ui/dashboard/supplies/ambulances/create/supply-form.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { useActionState, useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import { useState } from "react";
 import {
   AmbulanceAreaType,
   AmbulanceType,
+  CustomOptions,
   SupplyPharmacyType,
 } from "@/app/lib/definitions";
-import {
-  createSupplyInAmbulance,
-  SupplyInAmbulanceState,
-} from "@/app/lib/actions/supply";
-import { Button } from "@/app/ui/button";
+import { createSupplyInAmbulance } from "@/app/lib/actions/supply";
 import { FormInputSingle, FormSelect } from "@/app/ui/dashboard/form-fields";
 import { Badge } from "@/app/ui/badges";
+import { GenericForm } from "@/app/ui/components/GenericForm";
+import { getFormConfigFields } from "@/app/lib/config/formConfigs";
 
 export default function SupplyForm({
   ambulanceId,
@@ -40,7 +38,7 @@ export default function SupplyForm({
       id,
       value: id,
       label: category,
-    })
+    }),
   );
   const customSelectedAreas = areas.map(({ id, name }) => ({
     id,
@@ -48,24 +46,57 @@ export default function SupplyForm({
     label: name,
   }));
 
-  const initialState: SupplyInAmbulanceState = { errors: {}, message: null };
-  const [state, formAction] = useActionState(
-    createSupplyInAmbulance,
-    initialState
-  );
+  const fields = getFormConfigFields("supplyAmbulance", [
+    {
+      type: "select",
+      name: "ambulance",
+      title: "Ambulancia",
+      options: customSelectedAmbulances,
+      defaultValue: String(ambulanceId),
+      required: true,
+    },
+    {
+      type: "select",
+      name: "area",
+      title: "Área en la Ambulancia",
+      options: customSelectedAreas,
+      required: true,
+    },
+    {
+      type: "custom",
+      name: "customFiles",
+      component: CustomFiles({
+        suppliesPharmacy,
+        customSelectedSuppliesPharmacy,
+      }),
+    },
+    {
+      type: "number",
+      name: "minQuantity",
+      title: "Cantidad Mínima",
+      required: true,
+    },
+  ]);
 
+  return (
+    <GenericForm
+      fields={fields}
+      onSubmit={createSupplyInAmbulance}
+      initialState={{ errors: {}, message: null }}
+      onCancel={onClose}
+    />
+  );
+}
+
+function CustomFiles({
+  suppliesPharmacy,
+  customSelectedSuppliesPharmacy,
+}: {
+  suppliesPharmacy: SupplyPharmacyType[];
+  customSelectedSuppliesPharmacy: CustomOptions[];
+}) {
   const [quantity, setQuantity] = useState<number | null>(null);
   const [avaibleQuantity, setAvaibleQuantity] = useState<number | null>(null);
-
-  useEffect(() => {
-    state.message && toast.success(state.message);
-    state.message && onClose && onClose();
-  }, [state.message]);
-
-  useEffect(() => {
-    state.errors?.success &&
-      state.errors?.success.map((error: string) => toast.error(error));
-  }, [state.errors?.success]);
 
   function handleSupply(name: string, supplyId: string) {
     const { avaible_quantity } =
@@ -84,112 +115,51 @@ export default function SupplyForm({
       }
     }
   }
-
   return (
-    <form action={formAction}>
-      <div className="grid gap-4 mb-4 sm:grid-cols-1">
-        <FormSelect
-          key="select-ambulance"
-          name="ambulance"
-          title="Ambulancia"
-          options={[
-            {
-              id: 0,
-              label: "Seleccione un Ambulancia",
-              value: "",
-            },
-            ...customSelectedAmbulances,
-          ]}
-          defaultValue={ambulanceId}
-          errors={state.errors?.ambulanceId}
-          required
-        />
+    <>
+      <FormSelect
+        key="select-supply"
+        name="supply"
+        title="Insumo"
+        options={[
+          {
+            id: 0,
+            label: "Seleccione un Insumo",
+            value: "",
+          },
+          ...customSelectedSuppliesPharmacy,
+        ]}
+        handleOption={handleSupply}
+        required
+      />
 
-        <FormSelect
-          key="select-supply"
-          name="supply"
-          title="Insumo"
-          options={[
-            {
-              id: 0,
-              label: "Seleccione un Insumo",
-              value: "",
-            },
-            ...customSelectedSuppliesPharmacy,
-          ]}
-          errors={state.errors?.supplyId}
-          handleOption={handleSupply}
-          required
-        />
-
-        <FormSelect
-          key="select-area"
-          name="area"
-          title="Área en la Ambulancia"
-          options={[
-            {
-              id: 0,
-              label: "Seleccione el Área",
-              value: "",
-            },
-            ...customSelectedAreas,
-          ]}
-          errors={state.errors?.areaId}
-          required
-        />
-
-        {avaibleQuantity && (
-          <div className="w-5/12 space-y-2 md:w-3/12">
-            <p>Disponibilidad</p>
-            <Badge
-              title={String(quantity)}
-              success={quantity ? quantity > 5 : false}
-              pending={quantity ? quantity <= 5 : undefined}
-            />
-          </div>
-        )}
-
-        <div className="w-3/12 md:w-2/12">
-          <FormInputSingle
-            key={`input-avaibleQuantity-${avaibleQuantity}`}
-            type="number"
-            name="avaibleQuantity"
-            title="Cantidad"
-            placeholder="0"
-            initialValue={String(0)}
-            errors={state.errors?.avaibleQuantity}
-            handleOption={handleAvaibleQuantitySupply}
-            required
+      {avaibleQuantity && (
+        <div className="w-6/12 md:w-5/12">
+          <p className="mt-4 mb-4 text-sm font-medium text-gray-900 dark:text-white block">
+            Disponibilidad
+          </p>
+          <Badge
+            title={String(quantity)}
+            success={quantity ? quantity > 5 : false}
+            pending={quantity ? quantity <= 5 : undefined}
           />
         </div>
+      )}
 
-        <div className="w-5/12 md:w-3/12">
-          <FormInputSingle
-            key="input-minQuantity"
-            type="number"
-            name="minQuantity"
-            title="Cantidad Mínima"
-            placeholder="0"
-            errors={state.errors?.minQuantity}
-            required
-          />
-        </div>
+      <div></div>
+      <div className="w-5/12 md:w-4/12">
+        <FormInputSingle
+          key={`input-avaibleQuantity-${avaibleQuantity}`}
+          type="number"
+          name="avaibleQuantity"
+          title="Cantidad"
+          placeholder="0"
+          initialValue={String(0)}
+          handleOption={handleAvaibleQuantitySupply}
+          required
+        />
       </div>
-      <div className="w-full flex justify-end gap-4">
-        <Button
-          type="reset"
-          onMouseDown={onClose}
-          className="text-white inline-flex items-center bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Cancelar
-        </Button>
-        <Button
-          type="submit"
-          className="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        >
-          Guardar
-        </Button>
-      </div>
-    </form>
+      {!avaibleQuantity && <div></div>}
+    </>
   );
 }
